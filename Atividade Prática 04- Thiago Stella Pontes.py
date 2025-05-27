c05836a205a52531cf58eeab3912c2a05d65f89b
thiagoStella <thiagostellapontes01@gmail.com>
2025-05-27 20:50:46 -0300

    conclusao

--- Atividade Prática 04- Thiago Stella Pontes.py
+++ Atividade Prática 04- Thiago Stella Pontes.py
@@ -34,8 +34,11 @@ while True:
     else:
         print("Operação não encontrada!!")
         
-
     print(resultado)
+
+
+input("Encerrar?")
+
 # 2 Crie um programa para o professor registrar as notas da turma.
 
 soma = 0
@@ -67,3 +70,59 @@ else:
 print("Programa encerrado.")
 
 input("Encerrar?")
+
+# 3 senha 
+
+while True:
+    senha = input("Digite sua senha (pelo menos 8 caracteres e 1 número, ou 'sair' para encerrar): ")
+
+    if senha.lower() == 'sair':
+        print("Saindo do programa.")
+        break
+
+    if len(senha) < 8:
+        print("Senha fraca: A senha deve ter pelo menos 8 caracteres.")
+        continue
+
+    tem_numero = False
+    for caractere in senha:
+        if caractere.isdigit():
+            tem_numero = True
+            break
+
+    if not tem_numero:
+        print("Senha fraca: A senha deve conter pelo menos um número.")
+        continue
+
+    print("Senha forte! Senha aceita.")
+    break
+
+input("continuar?")
+
+
+        
+
+# Crie um programa que solicite números inteiros ao usuário.  Se não for um número inteiro, informar o erro e continuar.
+par = 0
+impar = 0
+num = 0
+while True:
+    num = input("Digite o numero:")
+    if num == "fim":
+        break
+    if num.isdigit():
+        num = int(num)
+        if num % 2 == 0:
+            print("Este numero é par!")
+            par += 1
+        else:
+            print("Este numero é impar!")
+            impar += 1
+    else: 
+        print("Entrada inválida! Por favor, digite um número inteiro positivo ou 'fim'.")
+
+print("Contém", par, "números pares e", impar, "ímpares.")
+
+
+
+input("Continuar?")
