57062c4a3660e24da78a2020791eb44e0d30c328
thiagoStella <thiagostellapontes01@gmail.com>
2025-05-30 12:28:02 -0300

    palindromo

---
+++ ex 5.2.py
@@ -0,0 +1,23 @@
+palavra = input("Digite a palavra a ser verificada: ")
+palavra = palavra.lower()
+palavra = palavra.replace(" ", "")
+palavra = palavra.replace(",", "")
+palavra = palavra.replace(".", "")
+palavra = palavra.replace("!", "")
+palavra = palavra.replace("?", "")
+palavra = palavra.replace("@", "")
+palavra = palavra.replace("ã", "a")
+palavra = palavra.replace("á", "a")
+palavra = palavra.replace("ê", "e")
+palavra = palavra.replace("é", "e")
+palavra = palavra.replace("í", "i")
+palavra = palavra.replace("ó", "o")
+palavra = palavra.replace("õ", "o")
+invertida = palavra[::-1]
+
+if invertida == palavra:
+    print("\n\tSim\n")
+else:
+    print("\n\tNão\n")
+
+input("encerrar execução?")
